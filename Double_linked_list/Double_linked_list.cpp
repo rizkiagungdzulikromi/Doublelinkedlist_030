8e37d5919e10c64c9ada3697561a7c2bf49408de
rizkiagungdzulikromi <riskiagung3005@gmail.com>
2023-05-25 12:05:31 +0700

    menambahkan method void DoubleLinkedList searchData

--- Double_linked_list/Double_linked_list.cpp
+++ Double_linked_list/Double_linked_list.cpp
@@ -146,6 +146,19 @@ void DoubleLinkedList::hapus() {
 }
 void DoubleLinkedList::searchData() {
 	if (listEmpty() == true) {
+		cout << "\nlist is empty" << endl;
 	
+	}
+	Node* prev, * curr;
+	prev = curr = NULL;
+	cout << "\nEnter the roll number of the student record you want to serach: ";
+	int num;
+	cin >> num;
+	if (DoubleLinkedList::search(num, &prev, &curr) == false)
+		cout << "\nRecord not found" << endl;
+	else {
+		cout << "\nRecord found" << endl;
+		cout << "\nRoll number: " << curr->noMhs << endl;
+		cout << "\nName: " << curr->name << endl;
 	}
 }
