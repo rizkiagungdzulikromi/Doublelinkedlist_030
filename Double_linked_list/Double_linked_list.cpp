a511a99c73650679abb4e37dab841693964ce91a
rizkiagungdzulikromi <riskiagung3005@gmail.com>
2023-05-25 11:45:47 +0700

    menambahkan method ascending

--- Double_linked_list/Double_linked_list.cpp
+++ Double_linked_list/Double_linked_list.cpp
@@ -122,5 +122,10 @@ void DoubleLinkedList::descending() {
 		Node* currentNode = START;
 		while (currentNode->next != NULL)
 			currentNode = currentNode->next;
+
+		while (currentNode != NULL) {
+			cout << currentNode->noMhs << " " << currentNode->name << endl;
+			currentNode = currentNode->prev;
+		}
 	}
 }
