a09d81df1dbfd47e0bfe38b58588d169361bf0f2
rizkiagungdzulikromi <riskiagung3005@gmail.com>
2023-05-25 11:41:03 +0700

    menambahkan doublelinkedlist bool

--- Double_linked_list/Double_linked_list.cpp
+++ Double_linked_list/Double_linked_list.cpp
@@ -101,7 +101,26 @@ bool DoubleLinkedList::listEmpty() {
 	return (START == NULL);
 }
 
-void DoubleLInkedList::ascending() {
+void DoubleLinkedList::ascending() {
 	if (listEmpty())
-		cout 
+		cout << "\nList is empty" << endl;
+	else {
+		cout << "\nRecords is ascending order of roll number are:" << endl;
+		Node* currentNode = START;
+		while (currentNode != NULL) {
+			cout << currentNode->noMhs << " " << currentNode->name << endl;
+			currentNode = currentNode->next;
+		}
+	}
+}
+
+void DoubleLinkedList::descending() {
+	if (listEmpty())
+		cout << "\nList is empty" << endl;
+	else {
+		cout << "\nRecords in descending order of roll number are:" << endl;
+		Node* currentNode = START;
+		while (currentNode->next != NULL)
+			currentNode = currentNode->next;
+	}
 }
